6bcfdb910604522117410f967d4e092a0a438c94
mariaprang <mprangishvili@newwork.com>
2024-06-22 19:24:10 +0400

    Initial commit

---
+++ src/main/java/org/app/TaskManagerSpringBootProjApplication.java
@@ -0,0 +1,13 @@
+package org.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskManagerSpringBootProjApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaskManagerSpringBootProjApplication.class, args);
+    }
+
+}
